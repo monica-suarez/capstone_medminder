b919d258d48e7d98ef9e699b0b8e0b6a04b573e4
monica-suarez <shutterbugmo@gmail.com>
2020-11-11 14:26:38 -0600

    ak done

--- client/src/Components/MedList/MedList.js
+++ client/src/Components/MedList/MedList.js
@@ -1,7 +1,10 @@
 import React from "react";
+// import { Box, Paper } from "@material-ui/core";
 import Header from "../Header";
 
 const MedList = () => {
+  const key = process.env.API_KEY;
+  console.log(typeof key);
   return (
     <div>
       <header className="page-header">
