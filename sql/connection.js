639a3d368f9f25d5e31f5227f09c00e32557b2af
monica-suarez <shutterbugmo@gmail.com>
2020-11-16 11:45:35 -0600

    added db pw .env

--- sql/connection.js
+++ sql/connection.js
@@ -1,32 +1,26 @@
-// const mysql = require('mysql')
+const mysql = require('mysql')
+require('dotenv').config();
 
-// class Connection {
-//   constructor() {
-//     if (!this.pool) {
-//       console.log('creating mysql connection...')
+console.log(process.env.DB_PASSWORD,);
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log('creating connection...')
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: '34.69.134.247',
+        user: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'hackathon311'
+      })
 
-//       const config = {
-//         connectionLimit: 100,
-//         host: '23.236.62.62',
-//         user: 'root',
-//         password: 'root',
-//         database: 'admin'
-//       }
+      return this.pool
+    }
 
-//       if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
-//         console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`)
-//         config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`
-//       }
+    return this.pool
+  }
+}
 
-//       this.pool = mysql.createPool(config)
+const instance = new Connection()
 
-//       return this.pool
-//     }
-
-//     return this.pool
-//   }
-// }
-
-// const instance = new Connection()
-
-// module.exports = instance;
+module.exports = instance;
