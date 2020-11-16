8a96043aa75791b1f8496c6fe985be4058b55fde
monica-suarez <shutterbugmo@gmail.com>
2020-11-16 17:13:43 -0600

    switched db

--- sql/connection.js
+++ sql/connection.js
@@ -1,26 +1,26 @@
-const mysql = require('mysql')
-require('dotenv').config();
+const mysql = require("mysql");
+require("dotenv").config();
 
-console.log(process.env.DB_PASSWORD,);
+console.log(process.env.DB_PASSWORD);
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating connection...')
+      console.log("creating connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '34.69.134.247',
-        user: 'root',
+        host: "34.69.134.247",
+        user: "root",
         password: process.env.DB_PASSWORD,
-        database: 'hackathon311'
-      })
+        database: "admin",
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
