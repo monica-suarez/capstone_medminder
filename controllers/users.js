716b1a152dec1194002cad5af41a7e5fb221748f
monica-suarez <shutterbugmo@gmail.com>
2020-11-23 16:39:45 -0600

    yaml file added

--- controllers/users.js
+++ controllers/users.js
@@ -56,34 +56,34 @@ const getUserById = (req, res) => {
 //   });
 // };
 
-// const createUser = (req, res) => {
-//   const {
-//     firstName,
-//     middleName,
-//     lastName,
-//     dateOfBirth,
-//     email,
-//     phone,
-//     username,
-//     password,
-//   } = req.body;
-//   let sql =
-//     "INSERT INTO users (first_name, middle_name, last_name, date_of_birth, email, phone, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-//   sql = mysql.format(sql, [
-//     firstName,
-//     middleName,
-//     lastName,
-//     dateOfBirth,
-//     email,
-//     phone,
-//     username,
-//     password,
-//   ]);
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err);
-//     return res.json({ newId: results.insertId });
-//   });
-// };
+const createUser = (req, res) => {
+  const {
+    firstName,
+    middleName,
+    lastName,
+    dateOfBirth,
+    email,
+    phone,
+    username,
+    password,
+  } = req.body;
+  let sql =
+    "INSERT INTO users (first_name, middle_name, last_name, date_of_birth, email, phone, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  sql = mysql.format(sql, [
+    firstName,
+    middleName,
+    lastName,
+    dateOfBirth,
+    email,
+    phone,
+    username,
+    password,
+  ]);
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+    return res.json({ newId: results.insertId });
+  });
+};
 
 const updateUserById = (req, res) => {
   const {
@@ -107,7 +107,6 @@ const updateUserById = (req, res) => {
     phone,
     username,
     password,
-    req.params.id,
   ]);
 
   pool.query(sql, (err, results) => {
